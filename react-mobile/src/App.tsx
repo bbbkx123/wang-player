bcbdc2549b3e1f1fa6cadeb39701e5ccd3852458
bbbkx <33077574+bbbkx123@users.noreply.github.com>
2021-02-02 10:58:47 +0800

    react-mobile搭建

---
+++ react-mobile/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button, WhiteSpace } from "antd-mobile";
+
+const App = (props: any) => {
+
+  return (
+    <div>
+      <Button>default</Button><WhiteSpace />
+    </div>
+  )
+}
+
+export default App
