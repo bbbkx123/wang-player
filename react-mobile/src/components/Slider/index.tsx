3c98f172d7db7d5dce039c86734c9d19af181a28
bbbkx <33077574+bbbkx123@users.noreply.github.com>
2021-04-19 15:41:59 +0800

    update: banner 添加

--- react-mobile/src/components/Slider/index.tsx
+++ react-mobile/src/components/Slider/index.tsx
@@ -3,19 +3,22 @@ import { useState, useRef, useEffect } from "react";
 import BetterScroll from "@better-scroll/core";
 import Slide from "@better-scroll/slide";
 
+import "./index.less"
+
 BetterScroll.use(Slide);
 
 const Slider = (props: any) => {
-  let BSInstance = null;
+  let BSInstance: any = null;
   const {children} = props
 
   const sliderRef = useRef<any>(null);
   const sliderGroupRef = useRef<any>(null);
 
   const [currentPageIndex, setCurrentPageIndex] = useState(0);
-  const [dots, setDots] = useState([]);
+  const [dots, setDots] = useState<number[]>([]);
 
   const initSlider = () => {
+    if (BSInstance !== null) return
     BSInstance = new BetterScroll(sliderRef.current, {
       scrollX: true,
       scrollY: false,
@@ -25,26 +28,41 @@ const Slider = (props: any) => {
         threshold: 0.3,
       },
     });
+    BSInstance.on('slideWillChange', (page: any) => {
+      setCurrentPageIndex(page.pageX)
+    })
   };
 
   const setSliderWidth = () => {
     let SliderWidth = sliderRef.current.clientWidth;
     let children = sliderGroupRef.current.children;
     let width = 0;
+    let _dots = []
 
-    for (let i = 0, len = children.length; i < len - 1; i++) {
+    for (let i = 0, len = children.length; i < len; i++) {
       let child = children[i];
       child.classList.add("slider-item");
       child.style.width = `${SliderWidth}px`;
       width += SliderWidth;
+      _dots.push(0)
     }
+    setDots(_dots.splice(0, _dots.length - 2))
 
     sliderGroupRef.current.style.width = `${width}px`;
   };
 
   useEffect(() => {
-    setSliderWidth()
-    initSlider()
+    // 存在children 为false的情况， 避免children不存在又创建BScroll实例
+    if (BSInstance === null && Array.isArray(children)) {
+      initSlider()
+      setSliderWidth()
+    }
+  }, [children])
+
+  useEffect(() => {
+    return () => {
+      
+    }
   }, [])
 
   return (
@@ -53,13 +71,18 @@ const Slider = (props: any) => {
         {children}
       </div>
       <div className="dots">
-        {dots.map((dot, index) => {
-          return (
-            <span
-              className={`${currentPageIndex === index ? "active" : ""}`}
-            ></span>
-          );
-        })}
+        <div className="dots-container">
+          {
+            dots.map((dot, index) => {
+              return (
+                <span
+                  className={`${currentPageIndex === index ? "active" : ""} dot`}
+                  key={`dot-${index}`}
+                ></span>
+              );
+            })
+          }
+        </div>
       </div>
     </div>
   );
--- react-mobile/src/views/recommend/index.tsx
+++ react-mobile/src/views/recommend/index.tsx
@@ -1,41 +1,95 @@
-import {useState} from 'react'
-import {withRouter} from 'react-router-dom'
-import Slider from "@/components/Slider"
+import { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import Slider from "@/components/Slider";
 
-import * as api from "@/service"
+import * as api from "@/service";
 
 const Recommend = (props: any) => {
-  const {history} = props
-  const [bannerArr, setBannerArr] = useState([])
+  const { history } = props;
+  const [bannerArr, setBannerArr] = useState([]);
+  const [bannerActions, setBannerActions] = useState<any[]>([]);
+  const [currentIndexOfBanner, setCurrentIndexOfBanner] = useState<number>(0);
   const fun1 = () => {
-    history.push("/playlistdetails")
-  }
+    history.push("/playlistdetails");
+  };
 
   const getBanner = () => {
-    let type = 0
+    let type = 0;
     api.getBanner(type).then((res) => {
       if (res.data.code === 200) {
-        setBannerArr(res.data.banners)
+        setBannerArr(res.data.banners);
       }
-    })
-  }
+    });
+  };
+
+  const handleBannerInit = (bannerArr: any[]) => {
+    return bannerArr.map(() => {
+      return { prev: false, current: false, next: false };
+    });
+  };
+
+  const handleBannerAutoScroll = () => {
+    // if (!this.timer) {
+    //   // console.log(this.timer)
+    //   this.timer = setInterval(() => {
+    //     this.handleBannerScrollForNextBtn()
+    //     // console.log('auto')
+    //   }, 5000)
+    // }
+  };
+
+  const handleBannerActive = (index: number) => {
+    const _bannerActions = handleBannerInit(bannerArr);
+    if (!_bannerActions) return console.log("bannerActions is null");
+    if (index === 0) {
+      _bannerActions[_bannerActions.length - 1].prev = true;
+      _bannerActions[0].current = true;
+      _bannerActions[1].next = true;
+    } else if (index === _bannerActions.length - 1) {
+      _bannerActions[_bannerActions.length - 2].prev = true;
+      _bannerActions[_bannerActions.length - 1].current = true;
+      _bannerActions[0].next = true;
+    } else {
+      _bannerActions[index - 1].prev = true;
+      _bannerActions[index].current = true;
+      _bannerActions[index + 1].next = true;
+    }
+    setBannerActions(_bannerActions);
+  };
+
+  useEffect(() => {
+    getBanner();
+  }, []);
+
+  useEffect(() => {
+    // ??? 长度小于3 需要处理
+    if (bannerArr.length > 2) {
+      setCurrentIndexOfBanner(0);
+      handleBannerActive(currentIndexOfBanner);
+      handleBannerAutoScroll();
+    }
+  }, [bannerArr]);
 
   return (
     <div>
       <button onClick={fun1}>click</button>
-      <div>
-        {/* <Slider>
-           {
-             bannerArr.map((banner, bIndex) => {
-               if (banner) {
-                 return <img :src="`${banner.imageUrl}?param=540y${bannerActions[bIndex].current ? 200 : 190}`" alt=""></Slider>
-               }
-             })
-           }
-        </Slider> */}
+      <div className="banner-container">
+        <Slider>
+          {bannerActions.length > 0 &&
+            bannerArr.map((banner: any, index: number) => {
+              if (banner) {
+                return (
+                  <img
+                    src={`${banner.imageUrl}?param=375y140`}
+                    key={`banner-${index}`}
+                  />
+                );
+              }
+            })}
+        </Slider>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default withRouter(Recommend)
+export default withRouter(Recommend);
