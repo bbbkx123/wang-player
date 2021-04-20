e62dad2ff7be68c64af95b677e0642df6006b37a
bbbkx <33077574+bbbkx123@users.noreply.github.com>
2021-04-20 15:18:07 +0800

    update: 列表详情页

--- react-mobile/src/components/List/define.ts
+++ react-mobile/src/components/List/define.ts
@@ -1,27 +1,29 @@
 export const label = [
   {
-    type: 'title',
+    type: 'name',
     name: '音乐标题',
     style: {
       width: '33%'
     }
-  }, {
-    type: 'singer',
+  }, 
+  {
+    type: 'artist',
     name: '歌手',
     style: {
       width: '23%'
     }
-  }, {
-    type: 'album',
-    name: '专辑',
-    style: {
-      width: '23%'
-    }
-  }, {
-    type: 'duration',
-    name: '时长',
-    style: {
-      width: 'calc(21% - 100px)'
-    }
-  }
+  }, 
+  // {
+  //   type: 'album',
+  //   name: '专辑',
+  //   style: {
+  //     width: '23%'
+  //   }
+  // }, {
+  //   type: 'duration',
+  //   name: '时长',
+  //   style: {
+  //     width: 'calc(21% - 100px)'
+  //   }
+  // }
 ]
--- react-mobile/src/views/PlayListDetails/index.tsx
+++ react-mobile/src/views/PlayListDetails/index.tsx
@@ -1,18 +1,55 @@
-import {useState, useContext, useEffect} from "react"
-import { StoreContext } from "@/store";
+import { useState, useContext, useEffect } from "react"
+import { StoreContext } from "@/store"
+import { fetchPlayListDetail, fetchSongsDetail } from "@/service/index"
+import { formatForPlayListDetail, formatForSong } from "@/utils/tools"
 
 import List from "@/components/List"
 
 const PlayListDetails = () => {
-  const {dispatch, playListDetail} = useContext<any>(StoreContext)
+  const { dispatch } = useContext<any>(StoreContext)
 
   const [listData, setListData] = useState([])
 
+  const getPlayListDetail = (detailId: string) => {
+    return fetchPlayListDetail(detailId)
+      .then((res: any) => {
+        const _res = formatForPlayListDetail(res)
+        const ids = _res.listData
+          .splice(0, 10)
+          .reduce((prev: any, cur: any) => prev.concat(cur, [","]), [])
+          .slice(0, -1)
+        return Promise.resolve(ids)
+      })
+      .then((ids) => {
+        return fetchSongsDetail(ids.join("")).then((res: any) => {
+          const _ids = ids.join("").split(",")
+          let listData = res.data.songs.map((item: any, index: number) => formatForSong(item, _ids[index]))
+          const value = {
+            ...res.data,
+            detailId,
+            listData,
+          }
+          delete value.code
+
+          dispatch({
+            type: "playListDetail",
+            value,
+          })
+          return Promise.resolve(value)
+        })
+      })
+  }
+
   useEffect(() => {
-    if (playListDetail) {
-      setListData(playListDetail.listData)
-    } 
-  }, [playListDetail])
+    console.log("create");
+    getPlayListDetail("129219563").then(res => {
+      setListData(res.listData)
+    })
+    return () => {
+      console.log("destory");
+      
+    }
+  }, [])
 
   return (
     <div>
